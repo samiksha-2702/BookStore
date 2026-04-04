e60d781e9c579694bb78bca4910d305b744cc458
Samiksha Apaka <samikshaapake@gmail.com>
2026-04-04 14:27:48 +0530

    Bookstore project setup

---
+++ cart/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+# class Cart(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# class CartItem(models.Model):
+#     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+#     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+#     quantity = models.IntegerField()
---
+++ orders/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class Order(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     status = models.CharField(max_length=50)
+#     created_at = models.DateTimeField(auto_now_add=True)
