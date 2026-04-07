afae7d16218aebb2183bbff56c4eb2a783dceb3b
Samiksha Apaka <samikshaapake@gmail.com>
2026-04-07 12:25:48 +0530

    added pagination

--- books/views.py
+++ books/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.core.paginator import Paginator
 from .models import Book, Category
 
 def book_list(request):
     books = Book.objects.all()
     categories = Category.objects.all()
 
-    # 🔍 Search
     query = request.GET.get('q')
     if query:
         books = books.filter(title__icontains=query)
 
-    # 🏷️ Category filter
     category = request.GET.get('category')
     if category:
         books = books.filter(category_id=category)
 
-    # 🔽 Sort
     sort = request.GET.get('sort')
     if sort == 'low':
         books = books.order_by('price')
     elif sort == 'high':
         books = books.order_by('-price')
 
+    paginator = Paginator(books, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'books/book_list.html', {
-        'books': books,
+        'books': page_obj,
         'categories': categories
     })
 
